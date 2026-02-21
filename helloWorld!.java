956abf54c41591b30a6be1ee461fe0dcbdfee0cd
Disyun <Lavemangus@gmail.com>
2026-02-21 16:14:23 +0300

    number operation

--- helloWorld!.java
+++ helloWorld!.java
@@ -1,7 +1,13 @@
-class App {        public static void main(String[] args) {
-                System.out.println("Hello, World!");
-                System.out.println("Mother of Dragons");
-                System.out.println("Dracarys!");
-                System.out.println("alala");
+// class App {        public static void main(String[] args) {
+//                 System.out.println("Hello, World!");
+//                 System.out.println("Mother of Dragons");
+//                 System.out.println("Dracarys!");
+//                 System.out.println("alala");
+//         }
+// }
+
+class App {
+        public static void main(String[] args) {
+                System.out.println(3 + 4);
         }
 }
