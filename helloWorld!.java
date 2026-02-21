744e0d92a68f7aaa9a1cf040d50d5940ab9d3517
Disyun <Lavemangus@gmail.com>
2026-02-21 16:59:13 +0300

    Escape characters

--- helloWorld!.java
+++ helloWorld!.java
@@ -14,6 +14,7 @@ class App {
                 System.out.println( 7 % 2);
                 System.out.println(0.2 + 0.1);
                 System.out.println(Integer.MAX_VALUE);
+                System.out.println("Dragon's mother said \"No\"");
         }
 }
 
