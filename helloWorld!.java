62a7cd27fdacde6e490ba37b403e50aeafc12421
Disyun <Lavemangus@gmail.com>
2026-02-21 16:09:15 +0300

    add new output line

--- helloWorld!.java
+++ helloWorld!.java
@@ -2,5 +2,6 @@ class App {        public static void main(String[] args) {
                 System.out.println("Hello, World!");
                 System.out.println("Mother of Dragons");
                 System.out.println("Dracarys!");
+                System.out.println("alala");
         }
 }
