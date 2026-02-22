42d57b5e4c0848c4fe8778350005781379fd210b
Disyun <Lavemangus@gmail.com>
2026-02-22 23:36:57 +0300

    trying variable

--- helloWorld!.java
+++ helloWorld!.java
@@ -1,4 +1,5 @@
-// class App {        public static void main(String[] args) {
+// class App {
+b//         public static void main(String[] args) {
 //                 System.out.println("Hello, World!");
 //                 System.out.println("Mother of Dragons");
 //                 System.out.println("Dracarys!");
@@ -6,15 +7,21 @@
 //         }
 // }
 
+// class App {
+//         public static void main(String[] args) {
+//                 System.out.println(3 + 4);
+//                 System.out.println(8 / 2);
+//                 System.out.println(3 * 3 * 3);
+//                 System.out.println( 7 % 2);
+//                 System.out.println(0.2 + 0.1);
+//                 System.out.println(Integer.MAX_VALUE);
+//                 System.out.println("Dragon's mother said \"No\"");
+//         }
+// }
+
 class App {
         public static void main(String[] args) {
-                System.out.println(3 + 4);
-                System.out.println(8 / 2);
-                System.out.println(3 * 3 * 3);
-                System.out.println( 7 % 2);
-                System.out.println(0.2 + 0.1);
-                System.out.println(Integer.MAX_VALUE);
-                System.out.println("Dragon's mother said \"No\"");
+                var greeting = "Father!";
+                System.out.println(greeting);
         }
 }
-
