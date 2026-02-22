b4fa08b0f9153b31392c517b37cd64826d4bf766
Disyun <Lavemangus@gmail.com>
2026-02-23 01:05:48 +0300

    Currency convertor program

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,10 +1,19 @@
+//class App {
+//    public static void main(String[] args) {
+//        var greeting = "Father!";
+//        System.out.println(greeting);
+//        System.out.println(greeting);
+//        greeting = "Mother!";
+//        System.out.println(greeting);
+//        System.out.println(greeting);
+//    }
+//}
+
 class App {
     public static void main(String[] args) {
-        var greeting = "Father!";
-        System.out.println(greeting);
-        System.out.println(greeting);
-        greeting = "Mother!";
-        System.out.println(greeting);
-        System.out.println(greeting);
+        var rublesPerDollar = 75;
+        var dollarCount = 50 * 1.25;
+        var rublesCount = dollarCount * rublesPerDollar;
+        System.out.println("The price is " + rublesCount + " rubles");
     }
 }
