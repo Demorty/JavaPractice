b65e50d0e0aec8190ddcb0f38ed020b03a55c1d6
Disyun <Lavemangus@gmail.com>
2026-02-23 00:45:54 +0300

    changing variable

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -2,5 +2,9 @@ class App {
     public static void main(String[] args) {
         var greeting = "Father!";
         System.out.println(greeting);
+        System.out.println(greeting);
+        greeting = "Mother!";
+        System.out.println(greeting);
+        System.out.println(greeting);
     }
 }
