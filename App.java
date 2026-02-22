c61267ca2f78bacd1770405f786c7ecf8c83da7d
Disyun <Lavemangus@gmail.com>
2026-02-23 00:26:40 +0300

    rename the helloWorld! file to App.java

---
+++ App.java
@@ -0,0 +1,27 @@
+// class App {
+b//         public static void main(String[] args) {
+//                 System.out.println("Hello, World!");
+//                 System.out.println("Mother of Dragons");
+//                 System.out.println("Dracarys!");
+//                 System.out.println("alala");
+//         }
+// }
+
+// class App {
+//         public static void main(String[] args) {
+//                 System.out.println(3 + 4);
+//                 System.out.println(8 / 2);
+//                 System.out.println(3 * 3 * 3);
+//                 System.out.println( 7 % 2);
+//                 System.out.println(0.2 + 0.1);
+//                 System.out.println(Integer.MAX_VALUE);
+//                 System.out.println("Dragon's mother said \"No\"");
+//         }
+// }
+
+class App {
+        public static void main(String[] args) {
+                String greeting = "Father";
+                System.out.println(greeting);
+        }
+}
